e2dc1b1a3a0733784e31b95681e82e6b996b7a0f
congson99 <48781511+congson99@users.noreply.github.com>
2019-05-06 21:23:03 +0700

    Tạo hàm check state unpaidbill

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -226,7 +232,23 @@ public class MainActivity extends AppCompatActivity {
         mData.child("User").setValue(usr); */
         //
 
+        //Xử lý sau khi cashier xác nhận bill
+        mData = FirebaseDatabase.getInstance().getReference();
+        mData.child("unpaidbill/state").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //TODO
+                
+                Toast.makeText(MainActivity.this, "Thanh toán thành công!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(MainActivity.this, "Thanh toán thất bại!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
 
 
 
